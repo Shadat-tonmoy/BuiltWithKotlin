181fe93e881dc3445aaed86b3e3f6c1cbc695235
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-05-31 11:12:48 +0600

    completed basic math operation using kotlin

---
+++ KotlinIntro/src/MathOperation.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    print("How Many Operations : ")
+    val n = readLine()!!.toInt()
+    for(i in 1..n)
+    {
+        print("Enter number 1 : ")
+        var number1 = readLine()!!.toDouble()
+        print("Enter number 2 : ")
+        var number2 = readLine()!!.toDouble()
+
+        println("Addition : ${number1+number2}")
+        println("Subtraction : ${number1-number2}")
+        println("Multiplication : ${number1 * number2}")
+        println("Division : ${number1 / number2}")
+    }
+
+
+}
