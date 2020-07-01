c4ba230fcbf7cf6d8039fd90f37020da4cbf5a87
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-07-01 22:23:03 +0600

    completed basic lambda functions syntax

--- KotlinIntro/src/LambdasAndHighLevelFunctions/01.Lambda.kt
+++ KotlinIntro/src/LambdasAndHighLevelFunctions/01.Lambda.kt
@@ -7,6 +7,24 @@ fun main()
         override fun onClick() { println("Clicked On Button") }
     })
 
+
+    //lambda function without defining any type
+    val testWithoutType = { n:String -> n.length > 10 }
+    //we can write the above lambda as
+    val testWithType : (String) -> Boolean = {n -> n.length > 10}
+    //lambda with multiple parameter
+    val isSubstring : (String, String) -> Boolean = {a,b -> a.contains(b)}
+    //a little bit complex lambda
+    val isPalindrome : (String, String) -> Boolean = {a,b -> a.reversed() == b }
+    val getGenderFromChar : (Char) -> String = {genderChar -> if(genderChar=='M') "Male"
+    else if(genderChar=='F') "Female"
+    else "Unknown"}
+
+    println("Gender is ${getGenderFromChar('M')}")
+
+    //lambda function with signature as type of variable
+    val test : (Int) -> Unit = { n -> println("PrintingFromLambdaFunction $n") }
+    test(5)
 }
 
 class Button
