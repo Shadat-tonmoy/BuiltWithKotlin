ee506be1537633204a89e716c98e5c14594ad333
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-07-02 10:44:38 +0600

    added basic documentation for lambdas and high level funtion

--- KotlinIntro/src/LambdasAndHighLevelFunctions/01.Lambda.kt
+++ KotlinIntro/src/LambdasAndHighLevelFunctions/01.Lambda.kt
@@ -1,5 +1,20 @@
 package LambdasAndHighLevelFunctions
 
+
+/**
+ * Lambda and High Level functions are extensively used in android.
+ * High Level functions are functions or methods that can take other functions as parameter and/or can return
+ * other functions.
+ * If any function perform one of these two operation then it can be called as a high level function.
+ * Lambdas are functions in the form of expression. It is often similar to a variable which has a data type.
+ * Lambdas can be declared as of the followings syntax :
+ * - {method_param_with_data_type -> method_body} for example :
+ * - {a,b -> println(a+b)}
+ * If lambda returns something then it should be the last line of the method body.
+ * Lambda can be assigned to a variable also. Then the syntax will be :
+ * val x : (Int,Int) -> Unit = {a,b -> println(a+b)}
+ * here the (Int,Int) -> Unit refers to the type of x meaning that it takes two integer as parameter and return Unit
+ * */
 fun main()
 {
     val button = Button()
